6fd8a6f11e76433d53a2da82f3597fe29805b569
Zayyan Faizal <zfaizal2@illinois.edu>
2021-07-31 13:31:35 -0700

    configure callback and redirect URIs for deploy

--- frontend/pages/api/utils/auth0.js
+++ frontend/pages/api/utils/auth0.js
@@ -1,12 +1,13 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
-export default initAuth0({
+export default initAuth0(
+    {
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_SECRET,
   scope: 'openid profile',
-  redirectUri: 'http://localhost:3000/api/callback',
-  postLogoutRedirectUri: 'http://localhost:3000',
+  redirectUri: process.env.callbackURI,
+  postLogoutRedirectUri: process.env.redirectURI,
   session: {
     cookieSecret: process.env.SESSION_COOKIE_SECRET,
     cookieLifetime: 7200,
