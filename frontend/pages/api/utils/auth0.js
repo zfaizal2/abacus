3a7843030883831a1922710397a8eee84ffe5261
Zayyan Faizal <zfaizal2@illinois.edu>
2021-08-01 00:33:22 -0700

    dependencies fix for deploy

--- frontend/pages/api/utils/auth0.js
+++ frontend/pages/api/utils/auth0.js
@@ -1,13 +1,12 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
-
-export default initAuth0(
-    {
+console.log(process.env.callbackUri)
+export default initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_SECRET,
   scope: 'openid profile',
-  redirectUri: process.env.callbackURI,
-  postLogoutRedirectUri: process.env.redirectURI,
+  redirectUri: process.env.callbackUri,
+  postLogoutRedirectUri: process.env.redirectUri,
   session: {
     cookieSecret: process.env.SESSION_COOKIE_SECRET,
     cookieLifetime: 7200,
